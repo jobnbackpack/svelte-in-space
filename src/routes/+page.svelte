8d03125a6b996391e5bdacdf70ea020394aea943
Robin Janzen <rjjanzen@web.de>
2023-07-19 20:13:15 +0200

    feat: add material ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+	import Button, { Label } from '@smui/button';
+	import Card from '@smui/card';
+	import Banner, { Label as BannerLabel } from '@smui/banner';
+
+	let bannerOpen = true;
+</script>
+
+<Banner fixed open={bannerOpen} content$style="max-width: max-content;">
+	<BannerLabel slot="label">Welcome</BannerLabel>
+	<Button slot="actions">Thanks</Button>
+</Banner>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<Card padded>
+		<p>this is a great button</p>
+		<Button><Label>test</Label></Button>
+	</Card>
+</div>
