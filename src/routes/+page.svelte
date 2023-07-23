c1cce65112d1600151df9703a360a79cbaf67664
Robin Janzen <rjjanzen@web.de>
2023-07-23 13:24:17 +0200

    feat: cards in cells with grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import type { PageData } from './$types';
     import { onMount } from 'svelte';
     import LaunchCard from '$lib/components/LaunchCard.svelte';
+    import LayoutGrid, { Cell } from '@smui/layout-grid';
 
     export let data: PageData;
 
@@ -11,7 +12,11 @@
 </script>
 
 <div>
-    {#each data.list.results as item}
-        <LaunchCard launch={item} />
-    {/each}
+    <LayoutGrid>
+        {#each data.list.results as item}
+            <Cell span={6}>
+                <LaunchCard launch={item} />
+            </Cell>
+        {/each}
+    </LayoutGrid>
 </div>
